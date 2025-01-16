dbe29033551cc259988c737f6805ac7e4023aa3a
Sravani127 <devarakondasravani1999@gmail.com>
2025-01-16 15:25:58 -0600

    Delete src directory

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-// src/routes/index.js
-const express = require('express');
-const authRoutes = require('./authRoutes');
-
-const router = express.Router();
-
-router.use('/auth', authRoutes);
-
-module.exports = router;
